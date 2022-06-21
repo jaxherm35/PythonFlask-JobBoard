6b4d43a82259a7050b3fac2ff2a3389906b9efd1
jaxherm35 <jaxherm35@gmail.com>
2022-06-20 19:10:18 -0600

    fourth section

--- jobs/app.py
+++ jobs/app.py
@@ -7,16 +7,16 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g, 'connection', None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factory.Row
+    connection.row_factory = sqlite3.Row
     return connection
 
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
-    cursor = connection.execute(sql,values)
+    cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
     else:
