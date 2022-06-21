777a6fe9265d115d31b394d31f86d6d71ccd9582
jaxherm35 <jaxherm35@gmail.com>
2022-06-20 18:39:51 -0600

    fourth section

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,33 @@
-from flask import Flask, render_template
+import sqlite3
+from webbrowser import get
+from flask import Flask, render_template, g
+
+PATH = 'db.jobs.sqlite3'
+
+def open_connection():
+    connection = getattr(g, 'connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+        connection.row_factory.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql,values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+        cursor.close()
+        return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, 'connection', None)
+    if connection is not None:
+        connection.close()
+
 
 app = Flask(__name__)
 
